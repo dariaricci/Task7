81c98eb35ee5323c90f7ffa9c33eef052760b863
Даша <@DESKTOP-K4NVIE4>
2022-02-02 10:26:14 +0300

    Добавьте файлы проекта.

---
+++ Task7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int z1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            int z2 = Convert.ToInt32(Console.ReadLine());
+
+            if (SumTriangle(x1, y1, z1) > SumTriangle(x2, y2, z2))
+                Console.WriteLine("Площадь первого треугольника больше");
+            else
+                Console.WriteLine("Площадь второго треугольника больше");
+            Console.ReadKey();
+        }
+        static double SumTriangle(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a)*(p - b)*(p - c));
+            return s;
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            CalcSV(N);
+            Console.ReadKey();
+        }
+        static void CalcSV(int a)
+        {
+            double S = 6 * Math.Pow(a, 2);
+            double V = Math.Pow(a, 3);
+            Console.WriteLine("Площадь поверхности куба = {0}",S);
+            Console.WriteLine("Объем куба = {0}", V);
+
+        }
+        
+
+    }
+
+}
